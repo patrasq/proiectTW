bc2b4cdc1e3bb3bfd58e7787ac115eb5dd98e103
Cosminelul2002 <cosminpatrascu611@yahoo.com>
2022-11-12 16:21:54 +0200

    [BE] Correct friend entity.

--- backend/src/user/entities/user.friend.entity.ts
+++ backend/src/user/entities/user.friend.entity.ts
@@ -4,14 +4,9 @@ import { User } from './user.entity';
 @Entity()
 export class UserFriend {
   @PrimaryGeneratedColumn({
-    name: 'user_id',
+    name: 'user_friend_id',
   })
-  userId: number;
-
-  @Column({
-    name: 'friend_id',
-  })
-  friendId: number;
+  id: number;
 
   @Column({
     name: 'friend_name',
