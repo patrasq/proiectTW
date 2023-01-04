3b1fd98e24b9771ea07ea900a36931ba3653b802
Andrei-Dragoș Pătrașcu <andreipatrasq@yahoo.com>
2023-01-04 16:29:55 +0200

    Boiler

--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,76 +1,84 @@
-<template>
-  <div class="container mx-auto px-4">
-    <h1 class="text-3xl font-bold text-center text-gray-800 mb-6">Sign Up</h1>
-    <form class="bg-white border bg-gray-50 rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="handleSubmit">
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-          Username
-        </label>
-        <input v-model="form.username" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
-      </div>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-          Email
-        </label>
-        <input v-model="form.email" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email">
-      </div>
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-          Password
-        </label>
-        <input v-model="form.password" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********">
-      </div>
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password_confirmation">
-          Confirm Password
-        </label>
-        <input v-model="form.password_confirmation" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password_confirmation" type="password" placeholder="********">
-      </div>
-      <div class="flex items-center justify-between">
-        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-          Sign Up
-        </button>
-        <a class="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-800" href="#">
-          Forgot Password?
-        </a>
-      </div>
-    </form>
-  </div>
-</template>
+	<template>
+	<div class="container mx-auto px-4">
+		<h1 class="text-3xl font-bold text-center text-gray-800 mb-6">Sign Up</h1>
+		<form class="bg-white border bg-gray-50 rounded px-8 pt-6 pb-8 mb-4" @submit.prevent="handleSubmit">
+		<div class="flex flex-row">
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+					First name
+				</label>
+				<input v-model="form.username" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+			</div>
+			<div class="mb-4">
+				<label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+					Last name
+				</label>
+				<input v-model="form.username" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
+			</div>
+		</div>
+		<div class="mb-4">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+			Email
+			</label>
+			<input v-model="form.email" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email">
+		</div>
+		<div class="mb-4">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+			Password
+			</label>
+			<input v-model="form.password" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="********">
+		</div>
+		<div class="mb-6">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="password_confirmation">
+			Confirm Password
+			</label>
+			<input v-model="form.password_confirmation" class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password_confirmation" type="password" placeholder="********">
+		</div>
+		<div class="flex items-center justify-between">
+			<button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+			Sign Up
+			</button>
+			<a class="inline-block align-baseline font-bold text-sm text-green-500 hover:text-green-800" href="#">
+			Forgot Password?
+			</a>
+		</div>
+		</form>
+	</div>
+	</template>
 
-<script>
-// include apiservice
-import api from '../services/apiService'
+	<script>
+	// include apiservice
+	import api from '../services/apiService'
 
-export default {
-  data() {
-    return {
-      form: {
-        username: '',
-        email: '',
-        password: '',
-        password_confirmation: ''
-      }
-    }
-  },
-  methods: {
-    handleSubmit() {
+	export default {
+	data() {
+		return {
+		form: {
+			username: '',
+			email: '',
+			password: '',
+			password_confirmation: ''
+		}
+		}
+	},
+	methods: {
+		handleSubmit() {
 
-      if (this.form.password !== this.form.password_confirmation) {
-        alert('Passwords do not match!')
-        return
-      }
+		if (this.form.password !== this.form.password_confirmation) {
+			alert('Passwords do not match!')
+			return
+		}
 
-      api.post('/register', this.form)
-        .then(response => {
-          console.log(response)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    }
-  }
-}
-</script>
+		api.post('/register', this.form)
+			.then(response => {
+			console.log(response)
+			})
+			.catch(error => {
+			console.log(error)
+			})
+		}
+	}
+	}
+	</script>
 
-       
+		
