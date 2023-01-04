81e720f02e75f28f8d4a1b8a35173a99f1c09555
Andrei-Dragoș Pătrașcu <andreipatrasq@yahoo.com>
2023-01-04 15:53:42 +0200

    Removed angular

--- frontend/src/app/modules/home/home.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from '@angular/router';
-import { HomePageComponent } from 'src/app/modules/home/components/home-page/home-page.component';
-
-export const homeRouting: Route[] = [
-	{
-		path: '',
-		component: HomePageComponent,
-	}
-]
